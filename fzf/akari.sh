68fd9d8ac0391ecea148f55af022dd6673fd5c0e
cappyzawa <cappyzawa@gmail.com>
2025-12-23 01:21:12 +0900

    Add fzf theme support
    Add color theme for fzf fuzzy finder with Akari palette.
    
    Signed-off-by: cappyzawa <cappyzawa@gmail.com>

---
+++ fzf/akari.sh
@@ -0,0 +1,6 @@
+export FZF_DEFAULT_OPTS="
+	--color=fg:#E6DED3,bg:#1C1A17,hl:#E26A3B
+	--color=fg+:#F2ECE4,bg+:#3A3530,hl+:#E26A3B
+	--color=border:#3A3530,header:#7A7A75,gutter:#1C1A17
+	--color=spinner:#E26A3B,info:#5A6F82
+	--color=pointer:#E26A3B,marker:#7FAF6A,prompt:#E26A3B"
